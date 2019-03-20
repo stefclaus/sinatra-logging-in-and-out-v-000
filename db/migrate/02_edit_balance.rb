5091a2a46ef0b188652c0271205cbbde0a3e048c
Stef Claus <stef.claus@gmail.com>
2019-03-20 21:02:49 +0000

    Automatically backed up by Learn

--- db/migrate/02_edit_balance.rb
+++ db/migrate/02_edit_balance.rb
@@ -1,12 +1,12 @@
 class EditBalance < ActiveRecord::Migration[4.2]
 
-  def change
-    create_table :users do |t|
-      t.string :username
-      t.string :password
-      t.integer :balance
-    end
-  end
+#  def change
+#    create_table :users do |t|
+#      t.string :username
+#      t.string :password
+#      t.integer :balance
+#    end
+#  end
 
 
 end
